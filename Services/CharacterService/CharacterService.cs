bbfacb7a0445d74caddc42fcdae13d0f8cd152b8
btopo <bret.topo@gmail.com>
2022-12-20 19:28:34 -0500

    Changed Put Implementations

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -76,15 +76,18 @@ namespace dotnet_Rpg6.Services.CharacterService
             
             try
             {
-            Character character = characters.FirstOrDefault(c => c.Id == updatedCharacter.Id);
+            var character = await _context.Characters
+            .FirstOrDefaultAsync(c => c.Id == updatedCharacter.Id);
 
             _mapper.Map(updatedCharacter, character);
-           // character.Name = updatedCharacter.Name;
-            //character.HitPoints = updatedCharacter.HitPoints;
-            //character.Strength = updatedCharacter.Strength;
-            //character.Defense = updatedCharacter.Defense;
-            //character.Intelligence = updatedCharacter.Intelligence;
-            //character.Class = updatedCharacter.Class;
+            character.Name = updatedCharacter.Name;
+            character.HitPoints = updatedCharacter.HitPoints;
+            character.Strength = updatedCharacter.Strength;
+            character.Defense = updatedCharacter.Defense;
+            character.Intelligence = updatedCharacter.Intelligence;
+            character.Class = updatedCharacter.Class;
+
+            await _context.SaveChangesAsync();
               
             response.Data = _mapper.Map<GetCharacterDto>(character);
             } 
