5d259e7c1da4ed763d9a5e09060a51b033362ef0
btopo <bret.topo@gmail.com>
2022-12-24 10:36:14 -0500

    Added verify Password Method and login method

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -20,12 +20,22 @@ namespace dotnet_Rpg6.Controllers
         }
 
 
-        [HttpPostAttribute("register")]   
+        [HttpPost("register")]   
         public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
         {
             var response = await _authRepo.Register(
                 new User {Username = request.Username}, request.Password
             );
+            if(!response.Success){
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+
+        [HttpPost("login")]   
+        public async Task<ActionResult<ServiceResponse<string>>> Login(UserLoginDto request)
+        {
+            var response = await _authRepo.Login(request.Username, request.Password);  
             if(!response.Success){
                 return BadRequest(response);
             }
