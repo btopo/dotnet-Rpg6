4620aad2e70eb5e065e004a1c1722ab411ae9a2d
btopo <bret.topo@gmail.com>
2023-01-19 21:53:26 -0500

    created random Fight method and FightRequestDto +more

--- Services/CharacterService/FightService/IFightService.cs
+++ Services/CharacterService/FightService/IFightService.cs
@@ -10,5 +10,7 @@ namespace dotnet_Rpg6.Services.CharacterService.FightService
     {
         Task<ServiceResponse<AttackResultDto>> WeaponAttack(WeaponAttackDto request);
         Task<ServiceResponse<AttackResultDto>> SkillAttack(SkillAttackDto request);
+
+        Task<ServiceResponse<FightResultDto>> Fight(FightRequestDto request);
     }
 }
